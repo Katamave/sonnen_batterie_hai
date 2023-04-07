c86bba828621ea7c64e34eaa97428ab6fb5ae428
Vaclav <katamave@gmail.com>
2023-04-07 18:55:09 +0200

    module first testing version with SOC and Status

--- custom_components/sonnen_batterie/sensor.py
+++ custom_components/sonnen_batterie/sensor.py
@@ -67,7 +67,7 @@ async def async_setup_platform(
     await data.async_update()
     sensors = [SonnenSensor(data, sensor_key, values) for sensor_key, values in DEFAULT_SENSORS.items()]
 
-    async_add_entities(sensors, update_before_add=False)
+    async_add_entities(sensors, update_before_add=True)
 
 
 class SonnenSensor(Entity):
