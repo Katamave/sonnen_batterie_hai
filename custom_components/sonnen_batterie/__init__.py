dbf915418bca8387ad6534ca894a705cf03167a3
Vaclav <katamave@gmail.com>
2023-04-07 14:19:39 +0200

    module first testing version with SOC and Status

--- custom_components/sonnen_batterie/__init__.py
+++ custom_components/sonnen_batterie/__init__.py
@@ -1,10 +1,10 @@
 from homeassistant import core
-from .const import DOMAIN
+#from .const import DOMAIN
 
 
-def setup(hass, config):
-    """Your controller/hub specific code."""
-    # Data that you want to share with your platforms
-    hass.data[DOMAIN] = {"x": 0}
-    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
-    return True
+#def setup(hass, config):
+#    """Your controller/hub specific code."""
+#    # Data that you want to share with your platforms
+#    hass.data[DOMAIN] = {"x": 0}
+#    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
+#    return True
